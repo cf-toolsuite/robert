560c0b1788ec3db45977765c2f741357bcc62857
Chris Phillipson <cphillipson@pivotal.io>
2024-11-08 16:01:57 -0800

    Add tmp to prune.sh

--- prune.sh
+++ prune.sh
@@ -4,7 +4,7 @@ export BACKUP_LOC=/tmp/sanford/$(date +"%Y.%m.%d.%N" | cut -b1-14)
 mkdir -p $BACKUP_LOC
 
 # Candidates to be pruned
-candidates=(".history" ".github" ".gradle" ".trunk" "config" "deploy-on-tp4cf.sh" "docs" "docker-compose.chroma.yml" "docker-compose.observability.yml" "docker-compose.pgvector.yml" "docker-compose.redis.yml" "docker-compose.weaviate.yml" "prometheus.yml" "bin" "build" "data" "samples" "README.md")
+candidates=(".history" ".github" ".gradle" ".trunk" "config" "deploy-on-tp4cf.sh" "docs" "docker-compose.chroma.yml" "docker-compose.observability.yml" "docker-compose.pgvector.yml" "docker-compose.redis.yml" "docker-compose.weaviate.yml" "prometheus.yml" "bin" "build" "data" "samples" "README.md" "tmp")
 
 for item in "${candidates[@]}"; do
     if [ -e "$item" ] || [ -d "$item" ] || compgen -G "$item" > /dev/null; then
